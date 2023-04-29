c6eee55fdc7489582bef91bfc340e1c2fe3c002b
sindaabidi <abidisinda28@gmail.com>
2023-04-29 23:28:24 +0100

    initial commit

---
+++ src/main/java/com/zns/erplight/ZedRegisterErurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.zns.erplight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // Indicate Eureka Server Application
+public class ZedRegisterErurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZedRegisterErurekaServerApplication.class, args);
+	}
+
+}
